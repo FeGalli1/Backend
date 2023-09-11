2704bd0422fbcfbea5fb8db82ee590a9c9260882
FeGalli1 <felipegall1.fg@gmail.com>
2023-09-11 18:37:19 -0300

    Primer Entrega backend

--- Class/ClaseProductos.js
+++ Class/ClaseProductos.js
@@ -2,28 +2,48 @@
 
 class ProductManage{
     constructor(){
-        this.productos=[
-            {
-                title,
-                description,
-                price,
-                thumbnail,
-                code,
-                stock
-            }
-        ]
+        this.productos=[];
+        this.Ids=1;
     }
 
     getProducts = () =>{
-        return this.productos
+        return this.productos;
     }
 
-    addProducts = (title,description,price,thumbnail,code,stock) => {
-        const newproduct = {title,description,price,thumbnail,code,stock}
-        return this.productos.push(newproduct)
+    addProduct = (title, description, price, thumbnail, code, stock) => {
+        if (title && description && price && thumbnail && code && stock) {
+                
+            const estaDuplicado = this.productos.some((producto) => producto.code === code);
+
+            if (!estaDuplicado) {
+                this.productos.push({ id:this.Ids,title, description, price, thumbnail, code, stock });
+                this.Ids++;
+            }else{
+                console.error("El codigo esta en uso, pruebe con uno distinto");
+            }
+        }else{
+            console.error("Una caracteristica del producto no fue agregada correctamente");
+        }
+    }
+
+    getProductById = (id) => {
+        const product = this.productos.find((producto) => producto.id === id);
+        if(product)
+        {
+            return product;
+        }else{
+            return "Not found";
+        }
     }
 
 }
 
-const productmanager= new ProductManage();
-console.log(productmanager.getProducts());
+
+/*  TE DEJO LOS COMANDO DE PRUEBA QUE YO REALICE */
+//const productmanager= new ProductManage();
+//console.log(productmanager.getProducts());
+//productmanager.addProduct("Prueba","Este es un producto prueba",32,"no imagen",129839,2);
+//console.log(productmanager.getProducts());
+//productmanager.addProduct("Prueba1","Este tambien es un producto prueba",62,"no imagen",129838,4);
+//console.log(productmanager.getProducts());
+//console.log(productmanager.getProductById(3));
